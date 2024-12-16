0af8538d49dad0581624738d8f430a808091afbc
Vitalina <148582544+vitalina-kostenko@users.noreply.github.com>
2024-12-17 02:13:42 +0300

    Update Form1.Designer.cs

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,39 +1,136 @@
-﻿namespace Lab3
+using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Lab3
 {
-    partial class Form1
+    public partial class Form1 : Form
     {
-        /// <summary>
-        ///  Required designer variable.
-        /// </summary>
-        private System.ComponentModel.IContainer components = null;
+        private Bitmap currentImage;
+        private PictureBox pictureBox;
+        private string imageFilePath; 
 
-        /// <summary>
-        ///  Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing && (components != null))
-            {
-                components.Dispose();
-            }
-            base.Dispose(disposing);
-        }
-
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.btnLoadImage = new Button();
+            this.btnFlipImage = new Button();
+            this.pictureBox = new PictureBox();
+            this.SuspendLayout();
+
+            // 
+            // btnLoadImage
+            // 
+            this.btnLoadImage.Location = new Point(20, 20);
+            this.btnLoadImage.Name = "btnLoadImage";
+            this.btnLoadImage.Size = new Size(120, 30);
+            this.btnLoadImage.Text = "Завантажити зображення";
+            this.btnLoadImage.Click += new EventHandler(this.BtnLoadImage_Click);
+
+            // 
+            // btnFlipImage
+            // 
+            this.btnFlipImage.Location = new Point(160, 20);
+            this.btnFlipImage.Name = "btnFlipImage";
+            this.btnFlipImage.Size = new Size(120, 30);
+            this.btnFlipImage.Text = "Перевернути";
+            this.btnFlipImage.Click += new EventHandler(this.BtnFlipImage_Click);
+
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new Point(20, 70);
+            this.pictureBox.Size = new Size(760, 360);
+            this.pictureBox.SizeMode = PictureBoxSizeMode.Zoom;
+
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.btnLoadImage);
+            this.Controls.Add(this.btnFlipImage);
+            this.Controls.Add(this.pictureBox);
+            this.Name = "Form1";
+            this.Text = "Зображення";
+            this.ResumeLayout(false);
         }
 
-        #endregion
+        private void BtnLoadImage_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image Files|*.bmp;*.gif;*.jpg;*.jpeg;*.png;*.tiff";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    imageFilePath = openFileDialog.FileName; // Store the file path
+                    if (IsImageValid(imageFilePath))
+                    {
+                        currentImage = new Bitmap(imageFilePath);
+                        pictureBox.Image = currentImage;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Недійсне зображення. Спробуйте вибрати інший файл.");
+                    }
+                }
+                catch (ArgumentException ex)
+                {
+                    MessageBox.Show($"Помилка завантаження зображення: {ex.Message}");
+                }
+            }
+        }
+
+        private bool IsImageValid(string filePath)
+        {
+            try
+            {
+                using (Bitmap testImage = new Bitmap(filePath)) { }
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private void BtnFlipImage_Click(object sender, EventArgs e)
+        {
+            if (currentImage != null && !string.IsNullOrEmpty(imageFilePath))
+            {
+                currentImage.RotateFlip(RotateFlipType.RotateNoneFlipX);
+                pictureBox.Image = currentImage;
+
+                string saveDirectory = @"C:\Users\koste\source\repos\Lab3_2\Lab3\-mirrored\";
+                if (!Directory.Exists(saveDirectory))
+                {
+                    Directory.CreateDirectory(saveDirectory);
+                }
+
+                string fileName = Path.GetFileNameWithoutExtension(imageFilePath); 
+                string fileExtension = Path.GetExtension(imageFilePath);
+                string savePath = Path.Combine(saveDirectory, fileName + "_mirrored" + fileExtension);
+
+                try
+                {
+                    currentImage.Save(savePath);
+                    MessageBox.Show($"Зображення успішно збережено: {savePath}");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Помилка при збереженні зображення: {ex.Message}");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Будь ласка, завантажте зображення перед перевертанням.");
+            }
+        }
+
+        private Button btnLoadImage;
+        private Button btnFlipImage;
     }
 }
+
