884255975ee8a92e2887876750fe6cae41774f76
Vitalina <148582544+vitalina-kostenko@users.noreply.github.com>
2024-12-17 02:13:03 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,62 +1,19 @@
-﻿using Lab3;
 using System;
-using System.Drawing;
-using System.IO;
-using System.Text.RegularExpressions;
 using System.Windows.Forms;
 
-class Program
+namespace Lab3
 {
-    [STAThread]
-    static void Main()
+    class Program
     {
-        Application.EnableVisualStyles();
-        Application.SetCompatibleTextRenderingDefault(false);
-        Application.Run(new Form1());
-
-        ProcessAndMirrorImages();
-    }
-
-    static void ProcessAndMirrorImages()
-    {
-        string projectDirectory = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName;
-        string inputDirectory = Path.Combine(projectDirectory, "file");
-
-        string outputDirectory = Path.Combine(projectDirectory, "-mirrored");
-
-        if (!Directory.Exists(outputDirectory))
+        [STAThread]
+        static void Main()
         {
-            Directory.CreateDirectory(outputDirectory);
-        }
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
 
-        Regex regexExtForImage = new Regex(@"((bmp)|(gif)|(tiff?)|(jpe?g)|(png))$", RegexOptions.IgnoreCase);
-
-        string[] files = Directory.GetFiles(inputDirectory);
-
-        foreach (var fileName in files)
-        {
-            try
-            {
-                if (regexExtForImage.IsMatch(Path.GetExtension(fileName)))
-                {
-                    using (Bitmap image = new Bitmap(fileName))
-                    {
-                        image.RotateFlip(RotateFlipType.RotateNoneFlipX);
-
-                        string newFileName = Path.Combine(outputDirectory,
-                            Path.GetFileNameWithoutExtension(fileName) + "-mirrored.gif");
-
-                        image.Save(newFileName, System.Drawing.Imaging.ImageFormat.Gif);
-                    }
-                }
-            }
-            catch (ArgumentException)
-            {
-                if (regexExtForImage.IsMatch(Path.GetExtension(fileName)))
-                {
-                    MessageBox.Show($"{fileName} не є зображенням.");
-                }
-            }
+            Form1 form1 = new Form1();
+            Application.Run(form1);
         }
     }
 }
+
